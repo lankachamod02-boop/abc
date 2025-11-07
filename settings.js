8735729c08cd34d39328648ec5e6263190aa2998
lankachamod02-boop <lankachamod02@gmail.com>
2025-11-07 21:39:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'a32bfc82d18004f2' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '1b5482c50df6c856' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
